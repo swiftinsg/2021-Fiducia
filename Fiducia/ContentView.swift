8b6c7506067d4a8d846462c926cea12ba6fe7d43
treepalms <lystreena@gmail.com>
2021-11-15 11:37:17 +0800

    Initial Commit

---
+++ Fiducia/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Fiducia
+//
+//  Created by sap on 15/11/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Fiducia/FiduciaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FiduciaApp.swift
+//  Fiducia
+//
+//  Created by sap on 15/11/21.
+//
+
+import SwiftUI
+
+@main
+struct FiduciaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
