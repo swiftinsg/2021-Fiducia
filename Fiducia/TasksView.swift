4d850809aaf85c8fb18a24ec780efc2aab576d61
winterxu-jm <rachelheer2005@gmail.com>
2021-11-19 12:08:11 +0800

    completed search bar and passed data from TasksData to TasksView

--- Fiducia/TasksView.swift
+++ Fiducia/TasksView.swift
@@ -13,85 +13,47 @@ struct TasksView: View {
     
     @State var isEditing = false
     
-    @State var searchText = ""
-
     @Binding var tasks: [Task]
     
-
+    @ObservedObject var tasksData = TasksData()
+    
+    @State var searchText = ""
+    
     var body: some View {
-        
-        
         NavigationView {
-            VStack {
-               HStack {
-         
-                    TextField("Search ...", text: $text)
-                        .padding(7)
-                        .padding(.horizontal, 25)
-                        .background(Color(.systemGray6))
-                        .cornerRadius(8)
-                        .padding(.horizontal, 10)
-                        .onTapGesture {
-                            self.isEditing = true
-                        }
-         
-                    if isEditing {
-                        Button(action: {
-                            self.isEditing = false
-                            self.text = ""
-         
-                        }) {
-                            Text("Cancel")
-                        }
-                        .padding(.trailing, 10)
-                        .transition(.move(edge: .trailing))
-                    }
-                }
-                .overlay(
-                    HStack {
-                        Image(systemName: "magnifyingglass")
-                            .foregroundColor(.gray)
-                            .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
-                            .padding(.leading, 15)
-                 
-                        if isEditing {
-                            Button(action: {
-                                self.text = ""
-                            }) {
-                            }
-                        }
-                    }
-                )
-                
-            List (0..<tasks.count - 1) { index in
-                ForEach(searchResult, id: \.self) { task in
-                        let taskIndex = tasks.firstIndex(of: task)!
+            List {
+                if searchText.isEmpty {
+                    ForEach($tasksData.tasks) { $task in
                         NavigationLink(destination:
-                                        TaskDetailedStepsView(task: $tasks[taskIndex]))
+                                        TaskDetailedStepsView(task: $task))
                         {
-                            Text(tasks[taskIndex].name)
+                            Text(task.name)
+                        }
+                    }
+                } else {
+                    let tasks = tasksData.tasks.filter { task in
+                        task.name.lowercased().contains(searchText.lowercased())
+                        
+                    }
+                    ForEach(tasks) { task in
+                        let taskIndex = tasksData.tasks.firstIndex(of: task)!
+                        NavigationLink(destination:
+                                        TaskDetailedStepsView(task: $tasksData.tasks[taskIndex]))
+                        {
+                            Text(task.name)
                         }
                     }
                 }
-            
-            }.navigationTitle("Tasks")
+                
+            }
+            .searchable(text: $searchText)
+            .navigationTitle("Tasks")
         }
     }
-    
-    var searchResult: [Task] {
-        if searchText.isEmpty {
-            return tasks
-        } else {
-            let searchArray = ["\(searchText)"]
-            let filteredArray = tasks.filter { searchArray.contains($0.name) }
-            return filteredArray
-        }
-    }
-    
 }
 
 struct TasksView_Previews: PreviewProvider {
     static var previews: some View {
-        TasksView(tasks: .constant([Task(name: "Make a ffcall", difficulty: 1, steps: ["hi"])]))
+        TasksView(tasks: .constant([Task(name: "Make a call", difficulty: 1, steps: ["hi"])]))
     }
 }
