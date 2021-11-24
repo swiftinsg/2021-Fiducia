4d512844a491a2066c5ab7bdcaa1bd00a69c3b0f
treena <lystreena@gmail.com>
2021-11-24 14:13:01 +0800

    user can delete/add steps

--- Fiducia/TaskSummarisedStepsView.swift
+++ Fiducia/TaskSummarisedStepsView.swift
@@ -18,17 +18,42 @@ struct TaskSummarisedStepsView: View {
     
     
     var body: some View {
-        
+        NavigationView {
+            
         List {
             TextEditor(text: $task.name)
-            
-            ForEach(1..<task.steps.count + 1) { index in
-                Text("Step \(index)")
-                    .bold()
-                    .padding()
-                TextEditor(text: $task.steps[index - 1])
+                .font(Font.title.weight(.bold))
+            Picker("Difficulty", selection: $task.difficulty){
+                Text("⭐")
+                    .tag("1")
+                Text("⭐⭐")
+                    .tag("2")
+                Text("⭐⭐⭐")
+                    .tag("3")
             }
-       }
+            .font(Font.headline.weight(.bold))
+            ForEach(1..<task.steps.count + 1, id: \.self) { index in
+                TextEditor(text: $task.steps[index - 1])
+                    .swipeActions(edge: .leading) {
+                        Button(action: {
+                            task.steps.remove(at: index - 1)
+                        }) {
+                            Image(systemName: "trash")
+                        }
+                        .tint(.red)
+                    }
+            }
+
+            Button(action: {
+                task.steps.append("")
+            }) {
+                Image(systemName: "plus")
+            }
+        }
+        .navigationBarHidden(true)
+            
+            
+        }
 
     }
 }
@@ -39,3 +64,13 @@ struct TaskSummarisedStepsView_Previews: PreviewProvider {
 }
 
 
+/*
+ .swipeActions(edge: .leading) {
+     Button(action: {
+         task.steps.remove()
+     }) {
+         Image(systemName: "trash")
+     }
+     .tint(.red)
+ }
+ */
